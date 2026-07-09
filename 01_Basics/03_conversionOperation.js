c5b5041e1a03acb0bf9c3b6ba71a628c8a7ac7db
suryanshyadav328-png <suryanshyadav328@gmail.com>
2026-07-09 07:50:05 +0000

    do

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -13,7 +13,7 @@ let valueInNumber = Number(score)
 
 let isLoggedIn = 1;
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
 
 // 1 => true; 0 => false
 // "" => false
@@ -22,4 +22,48 @@ console.log(booleanIsLoggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+// ******************* Operations *****************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2%2);
+// console.log(2/2);
+
+
+let str1 = "hello"
+let str2 = " hitesh"
+
+let str3 = str1 + str2
+// console.log(str3)
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+
+// console.log(1 + 2 + "2");
+
+// console.log(+true);
+// console.log(+"");
+
+let num1 , num2, num3
+
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+++gameCounter
+console.log((gameCounter));
+
+
+// link to study
+
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
+
