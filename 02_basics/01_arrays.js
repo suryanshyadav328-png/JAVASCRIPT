6fb8a68ede3b5400ea73aa2dd2ceb9c7350b1e60
suryanshyadav328-png <suryanshyadav328@gmail.com>
2026-07-10 15:05:15 +0000

    do

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,18 @@
+// Array
+
+const myArr = [0, 1, 2, 3, 4, 5, " suru"]
+
+const myHeros = ["ben10", "doraemon"]
+const myArr2 = new Array(1, 2, 3, 4)
+
+console.log(myArr[1]);
+
+
+// ARRAY METHODS
+
+// myArr.push(6)
+// myArr.pop()
+
+myArr.unshift(0)
+
+console.log(myArr);
