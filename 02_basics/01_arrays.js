0f906c0176233a5743c585a57fe7a187d0e02e95
suryanshyadav328-png <suryanshyadav328@gmail.com>
2026-07-10 15:22:04 +0000

    do

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -5,7 +5,7 @@ const myArr = [0, 1, 2, 3, 4, 5, " suru"]
 const myHeros = ["ben10", "doraemon"]
 const myArr2 = new Array(1, 2, 3, 4)
 
-console.log(myArr[1]);
+// console.log(myArr[1]);
 
 
 // ARRAY METHODS
@@ -13,6 +13,23 @@ console.log(myArr[1]);
 // myArr.push(6)
 // myArr.pop()
 
-myArr.unshift(0)
+// myArr.unshift(0)
+// myArr.shift()
 
-console.log(myArr);
+// console.log(myArr.includes(9)); //datatype : boolean
+// console.log(myArr.indexOf(9));   // -1
+
+// const newArr = myArr.join()
+
+// console.log(myArr);
+// console.log(newArr); // datatype : string
+
+
+// SLICE, SPLICE
+
+console.log("A", myArr);
+
+const myn1 = myArr.slice(1,3)
+
+console.log(myn1);
+console.log("B", myArr);
